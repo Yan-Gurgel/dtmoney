074d901f80da58b2c080e48ddc825fbb1a1d057f
Yan-Gurgel <yan.gurgel@gmail.com>
2021-04-05 14:24:21 -0300

    Listando transacoes e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,35 @@ import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
-
   models: {
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-04-05 14:00:00')
+        },
 
-  routes() { 
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-04-06 18:00:00')
+        }
+      ],
+    })
+  },
+
+  routes() {
     this.namespace = 'api'; //a partir do endereço /api
     //quando ocorre requisição do tipo get para a rota /transactions
     this.get('/transactions', () => {
